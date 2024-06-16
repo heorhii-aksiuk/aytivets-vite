c8ede79668d41ae2df39f3a7b931aadd67eed7c9
heorhii-aksiuk <zhorik044@gmail.com>
2024-06-16 22:46:37 +0300

    change welcome text

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,5 +1,5 @@
-import heroImg from '../../assets/images/hero.jpg';
-import qrImg from '../../assets/images/qr.png';
+import heroImg from "../../assets/images/hero.jpg";
+import qrImg from "../../assets/images/qr.png";
 
 function Hero() {
   return (
@@ -8,18 +8,17 @@ function Hero() {
         <img className="object-contain" src={heroImg}></img>
         <div className="bg-sky-400">
           <h1 className="-mt-16 text-4xl font-semibold text-slate-300/95 [text-shadow:_0_-2px_rgb(236_72_153_/_90%)]">
-            Heorhii Aksiuk{' '}
+            Heorhii Aksiuk{" "}
             <span className="block text-3xl text-pink-500 [text-shadow:_0_-2px_rgb(220_220_220_/_90%)]">
               Software Engineer
             </span>
           </h1>
-          <h2 className="text-white text-xl font-semibold">Hello world!</h2>
+          <h2 className="text-white text-xl font-semibold">Hello world!</h2>∄
           <p className="text-white">
-            My name is Heorhii aka Zhorik, Zhora, zhorik044, Aytivets... or even
-            Yura, Yurchyk... whatever. I'm into a lot of different stuff and
-            love cats😺. You can find me in a forest, ∄, or hardcore mosh pit.
-            Write me if you need a business card site like this or any web
-            application.
+            My name is Heorhii aka Zhorik, Zhora, zhorik044, Aytivets ...
+            whatever. I'm into a lot of different stuff and love cats😺. You can
+            find me in a forest, ∄ club, or a hardcore mosh pit. Write me if you
+            need any site or any web application.
           </p>
           <a
             href="https://t.me/aytivets"
